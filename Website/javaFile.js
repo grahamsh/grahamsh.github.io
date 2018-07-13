8f73c510ad52ee0b56f1160dc36eeba4da72dba5
grahamsh <harper.grahams@gmail.com>
2018-07-13 13:15:34 -0600

    Add files via upload

---
+++ Website/javaFile.js
@@ -0,0 +1,35 @@
+$('#pullout-tab').click(function() {
+  $('#pullout').toggleClass('active');
+});
+
+
+/* Open the sidenav */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+function openTab(evt, tab){
+	var i, tabcontent, tablinks;
+
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i=0; i<tabcontent.length; i++){
+		tabcontent[i].style.display = "none";
+	}
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+
